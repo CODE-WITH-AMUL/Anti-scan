317ec1fdfea334349f4bf70970ee57f44ac0ae18
SUGAR DADDY 🗿 <140779732+CODE-WITH-MASKMAN@users.noreply.github.com>
2024-06-27 10:19:59 +0545

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,31 +1,23 @@
-import os, platform
- 
+import os
+import platform
+
 try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-import requests
- 
+    import requests
+except ImportError:
+    os.system('pip install requests')
+
 bit = platform.architecture()[0]
- 
+
 if bit == "64bit":
- 
+    try:
         from VIRUS import Main
- 
-        Main()
- 
- 
- 
-elif bit == "32bit":
- 
+    except ImportError:
+        print("The module 'VIRUS' is not available.")
+else:
+    try:
         from VIRUS_SCAN import Main
- 
- 
-        Main()
- 
+    except ImportError:
+        print("The module 'VIRUS_SCAN' is not available.")
+
+if 'Main' in locals():
+    Main()
